aba9f609f32dc4608890b429c68a5f89b852ec52
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2022-07-09 19:24:48 -0300

    waifuim created

--- command/weebs/waifuim.js
+++ command/weebs/waifuim.js
@@ -1,4 +1,4 @@
-const { fetchText } = require("../../utils")
+const { fetchText, fetchJson } = require("../../utils")
 const { footer } = require("../../config.json");
 const { mimeTypes, fromStream } = require("file-type");
 const fs = require('fs');
@@ -6,64 +6,56 @@ const { slice } = require("cheerio/lib/api/traversing");
 const fetch = require('node-fetch');
 
 module.exports = {
-	name: "waifupics",
-	desc: "Use esse comando para receber imagens e gifs do site waifu.pics :)",
+	name: "waifim",
+	desc: "Use esse comando para receber imagens e gifs do site waifu.im :)",
 	use: "[modo] [categoria]\n\n- *modos* -\n\n1. nsfw\n2. sfw\n\n- *Categorias* -\n\n* sfw *:\nwaifu\nneko\nshinobu\nmegumin\nbully\ncuddle\ncry\nhug\nawoo\nkiss\nlick\npat\nsmug\nbonk\nyeet\nblush\nsmile\nwave\nhighfive\nhandhold\nnom\nbite\nglomp\nslap\nkill\nkick\nhappy\nwink\npoke\ndance\ncringe\n\n* nsfw *:\nwaifu\nneko\ntrap\nblowjob",
 	category: "weebs",
-	alias: ["wp", "waifu"],
+	alias: ["wi", "waifuim"],
 	async exec({ msg, sock, args, arg, isOwner, name }) {
-        let tipo = args[0]
-        let category = args[1]
+        let category = args[0]
         try { 
             //provavelmente tinha um jeito melhor de fazer isso que você está vendo, mas eu não sei como, no momento
-            function exten(check) { 
-                if (check.includes(".gif")) {
-                    let resultado = ".gif"
-                    return resultado
-                } else {
-                    let resultado = ".jpg"
-                    return resultado
-                }
-            }
 
             async function saveCache(){
                 //gera um nome aleatório pro arquivo
                 let result = Math.random().toString(36).substring(2,7);
                 //salva o arquivo
-                let response = await fetch(bjj.url);
+                let response = await fetch(bjj.images[0].url);
                 let buffer = await response.buffer();
-                let saved = fs.writeFile(`./event/cache/wp/${tipo}/${category}/${result}.${exten(bjj.url)}`, buffer, () => 
-                console.log(`Salvo como ${result}.${exten(bjj.url)}`));
+                let saved = fs.writeFile(`./event/cache/wi/${category}/${result}.${bjj.images[0].extension}`, buffer, () => 
+                console.log(`Salvo como ${result}.${bjj.images[0].extension}`));
                 return saved;
             }
             
             function legenda() {
-                if (bjj.url.includes(".gif")) {
-                    let caption = `Aqui está!\nCaso o gif não esteja se mexendo, veja ele aqui: ${bjj.url} :)`
+                if (bjj.images[0].extension == ".gif") {
+                    let caption = `Aqui está!\nCaso o gif não esteja se mexendo, veja ele aqui: ${bjj.images[0].url} :)`
                     return caption
                 } else {
-                    let caption = `Aqui está!\nFonte: ${bjj.url}`
+                    let caption = `Aqui está!\nFonte: ${bjj.images[0].url}`
                     return caption
                 }
             }
 
-            if (tipo == undefined || category == undefined) {
-                tipo = 'sfw'
-                category = 'neko'
+            if (category == undefined) {
+                category = 'waifu'
             }
-            let bjj = await fetchText(`https://api.waifu.pics/${tipo}/${category}`)
+            
+            let bjj = await fetchText(`https://api.waifu.im/random/?selected_tags=${category}`)
 
-            console.log(typeof(bjj.url))
+            let res = bjj.images[0].url
+
+            console.log(res)
 
             await sock.sendMessage(
                 msg.from,
                 {
-                    image: { url: bjj.url },
+                    image: { url: res },
                     caption: legenda()
                 },
                 { quoted: msg }
             );
-            saveCache();
+           saveCache();
         } catch (e) {
             msg.reply('Esse termo não existe ou não foi encontrado. Tente outro :)')
             console.log(e)
